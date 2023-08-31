2e749b0692a94be26b3bf83fa5b37cf29eab1c9f
Old-Sumerian <oldsumerian@proton.me>
2023-08-31 11:22:20 +0000

    code refactor.

--- src/components/service/face_detect.js
+++ src/components/service/face_detect.js
@@ -90,4 +90,67 @@ class FaceDetectService extends React.Component {
                 <Upload.Dragger name="file" accept=".jpg,.jpeg,.png" beforeUpload={(file)=>{ this.processFile(file); return false; }} >
                     <p className="ant-upload-drag-icon">
                         <Icon type="inbox" />
-    
+                    </p>
+                    <p className="ant-upload-text">Click for file-chooser dialog or drag a file to this area to be analyzed.</p>
+                </Upload.Dragger>
+            </React.Fragment>
+        }
+        <table><tbody>
+            <tr>
+                <td><b>File:</b></td>
+                <td>{this.state.file ? `${this.state.file.name}` : '(not uploaded)'}</td>
+            </tr>
+        </tbody>
+        </table>
+
+        <br/>
+        <br/>
+        {
+            this.state.fileUploaded &&
+            <img src={ this.state.fileReader.result } />
+        }
+        <br/>
+        <br/>
+        <Button type="primary" onClick={() => {this.submitAction(); }} disabled={!this.state.fileUploaded} >Call Agent API</Button>
+        </div>
+        </React.Fragment>
+        )
+  }
+  
+  renderComplete() {
+    let jsonResult = JSON.stringify(this.props.jobResult);
+    return(
+      <div>
+        <div>
+          <textarea rows="4" cols="50" readOnly value={jsonResult}/>
+        </div>
+        <div ref="outsideWrap" style={styles.outsideWrapper}>
+          <div style={styles.insideWrapper}>
+            <img ref="sourceImg" style={styles.coveredImage} src={this.state.fileReader.result}/>
+            <canvas ref="bboxCanvas" style={styles.coveringCanvas}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  renderDescription() {
+    return(
+      <div>
+          <p>
+          A service that detects the location of human faces and returns a 2d bounding box.
+          
+          This is part of the <a href="https://github.com/singnet/face-services">face-services</a> suite of example
+          SingularityNET services.
+          </p>
+      </div>
+    )
+  }
+
+  render() {
+    if (this.isComplete())
+        return (
+            <div>
+            { this.renderDescription() }
+            { this.renderComplete() }
+            
