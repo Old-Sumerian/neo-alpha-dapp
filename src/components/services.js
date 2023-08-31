c34d43e6713f10a1a1d37a7c73367b80e15c180a
Old-Sumerian <oldsumerian@proton.me>
2023-08-31 14:13:26 +0000

    PEP8 fixes

--- src/components/services.js
+++ src/components/services.js
@@ -28,4 +28,54 @@ class Services extends React.Component {
         render:     (address, agent, index) =>
           this.props.network &&
           <Tag>
-            <a target="_b
+            <a target="_blank" href={this.props.network && typeof NETWORKS[this.props.network] !== "undefined" ? `${NETWORKS[this.props.network].etherscan}/address/${address}` : undefined}>
+              {FORMAT_UTILS.toHumanFriendlyAddressPreview(address)}
+            </a>
+          </Tag>
+      },
+      {
+        title:      'Current Price',
+        dataIndex:  'currentPrice',
+        render:     (currentPrice, agent, index) => `${AGI.toDecimal(currentPrice)} AGI`,
+      },
+      {
+        title:      'Agent Endpoint',
+        dataIndex:  'endpoint',
+        width: '23ch'
+      },
+      {
+        title:      '',
+        dataIndex:  'state',
+        render:     (state, agent, index) =>
+          <Button type={state == AGENT_STATE.ENABLED ? 'primary' : 'danger'} disabled={ !(state == AGENT_STATE.ENABLED) || typeof this.props.account === 'undefined' || typeof this.state.selectedAgent !== 'undefined' } onClick={() => { this.setState({ selectedAgent: agent }); return this.props.onAgentClick(agent); }} >
+            { this.getAgentButtonText(state, agent) }
+          </Button>
+        }
+    ].map(column => Object.assign({}, { width: 150 }, column));
+
+    this.watchRegistriesTimer = undefined;
+  }
+
+  getAgentButtonText(state, agent) {
+    if (this.props.account) {
+      if (typeof this.state.selectedAgent === 'undefined' || this.state.selectedAgent.key !== agent.key) {
+        return state == AGENT_STATE.ENABLED ? 'Create Job' : 'Agent Disabled';
+      } else {
+        return 'Selected';
+      }
+    } else {
+      return 'Unlock account';
+    }
+  }
+
+  componentWillMount() {
+    this.watchRegistriesTimer = setInterval(() => this.watchRegistries(), 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.watchRegistriesTimer);
+  }
+
+  hexToAscii(hexString) { 
+    let asciiString = Eth.toAscii(hexString);
+    return asciiString.substr(0,asciiString.indexOf("\0")); 
